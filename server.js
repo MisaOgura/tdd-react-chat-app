41c203ebef5eea57e48642b618b47f32fad59a7e
MisaOgura <misa.ogura01@gmail.com>
2017-03-09 15:07:14 +0000

    Build basic client-server connection using express, http and socket.io

---
+++ server.js
@@ -0,0 +1,21 @@
+// While import is indeed part of ES6, it is unfortunately not
+// yet supported by any native environments, Node or browser.
+
+
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io').listen(server)
+
+io.on('connection', (socket) => {
+	console.log('client-server connection established')
+	socket.on('client-data', (data) => {
+		console.log(data)
+	})
+})
+
+server.listen('3030', () => {
+	console.log('Express server listening on port 3030...')
+})
